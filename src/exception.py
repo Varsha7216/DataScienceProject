ab75e3f67c7748f4c9eb60a5ef8b1e68df4b1b2b
Varsha7216 <varshakumari.6594@gmail.com>
2024-04-05 21:53:02 +0530

    This is after model training commit

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from logger import logging
 def error_message_details(error,error_detail:sys):
     _,_,exc_ed=error_detail.exc_info()
     file_name=exc_ed.tb_frame.f_code.co_filename
@@ -13,9 +13,10 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
-if __name__=="__main__":
+'''if __name__=="__main__":
     try:
         a=1/0
     except Exception as e:
         logging.info("Divide by 0 error")
-        raise CustomException(e,sys)
+
+        raise CustomException(e,sys)'''
